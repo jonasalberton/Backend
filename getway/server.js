d91cf6562b3b63c1f1cb0aea25a4ef648895f6ec
jonasalberton <jonasceolin@gmail.com>
2022-11-18 21:09:37 -0300

    auth middleware

--- getway/server.js
+++ getway/server.js
@@ -1,33 +1,39 @@
-const express = require('express');
-const httpProxy = require('express-http-proxy');
+const express = require("express");
+const httpProxy = require("express-http-proxy");
 const app = express();
 const port = 3000;
 
-const NODE_API = 'http://localhost:3332'
-const JAVA_API = 'http://localhost:3002'
+const NODE_API = "http://localhost:3332";
+const JAVA_API = "http://localhost:3002";
 
 const nodeProxy = httpProxy(NODE_API);
 const javaProxy = httpProxy(JAVA_API);
 
-app.get('/', (req, res) => res.send('Hello Gateway API'));
+function authMiddleware(req, res, next) {
+	// request authguard here
+  next();
+}
+
+app.use(authMiddleware);
+
+app.get("/", (req, res) => res.send("Hello Gateway API"));
 
 // Tabelas
-app.post('/tabelas', (req, res, next) => nodeProxy(req, res, next));
-app.put('/tabelas/:id', (req, res, next) => nodeProxy(req, res, next));
-app.delete('/tabelas/:id', (req, res, next) => nodeProxy(req, res, next));
-app.get('/tabelas/:id', (req, res, next) => nodeProxy(req, res, next));
-app.get('/tabelas', (req, res, next) => nodeProxy(req, res, next));
-
+app.post("/tabelas", (req, res, next) => nodeProxy(req, res, next));
+app.put("/tabelas/:id", (req, res, next) => nodeProxy(req, res, next));
+app.delete("/tabelas/:id", (req, res, next) => nodeProxy(req, res, next));
+app.get("/tabelas/:id", (req, res, next) => nodeProxy(req, res, next));
+app.get("/tabelas", (req, res, next) => nodeProxy(req, res, next));
 
 // Fretes
-app.get('/custos/frete', (req, res, next) => nodeProxy(req, res, next));
-
+app.get("/custos/frete", (req, res, next) => nodeProxy(req, res, next));
 
 // Entregas
-app.get('/entregas/:id', (req, res, next) => javaProxy(req, res, next));
-app.post('/entregas', (req, res, next) => javaProxy(req, res, next));
-app.get('/entregas/entregador/:id', (req, res, next) => javaProxy(req, res, next));
-app.pacth('/entregas/:id', (req, res, next) => javaProxy(req, res, next));
-
+app.get("/entregas/:id", (req, res, next) => javaProxy(req, res, next));
+app.post("/entregas", (req, res, next) => javaProxy(req, res, next));
+app.get("/entregas/entregador/:id", (req, res, next) =>
+  javaProxy(req, res, next)
+);
+app.patch("/entregas/:id", (req, res, next) => javaProxy(req, res, next));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
